9f62586bac3f71b7f3ee5714d878fb9682a8902b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-01-29 23:16:58 -0500

    added colorful console files

---
+++ src/week01/ColorfulConsole.java
@@ -0,0 +1,16 @@
+package week01;
+
+public class ColorfulConsole {
+
+    public static final String RESET = "\033[0m"; // Text reset
+    public static final String BLACK = "\033[0;30m-ZEHRA-"; // BLACK
+    public static final String RED = "\033[0;31m-ZEHRA-"; // RED
+    public static final String GREEN = "\033[0;32m-ZEHRA-"; // GREEN
+    public static final String YELLOW = "\033[0;33m-ZEHRA-"; // YELLOW
+    public static final String BLUE = "\033[0;34m-ZEHRA-"; // BLUE
+    public static final String PURPLE = "\033[0;35m-ZEHRA-"; // PURPLE
+    public static final String CYAN = "\033[0;36m-ZEHRA-"; // CYAN
+    public static final String WHITE = "\033[0;37m-ZEHRA-"; // WHITE
+}
+
+// How to print colorful output on console?
---
+++ src/week01/ColorfulConsoleTest.java
@@ -0,0 +1,18 @@
+package week01;
+
+public class ColorfulConsoleTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(ColorfulConsole.BLACK);
+        System.out.println(ColorfulConsole.YELLOW);
+        System.out.println(ColorfulConsole.WHITE);
+        System.out.println(ColorfulConsole.CYAN);
+        System.out.println(ColorfulConsole.PURPLE);
+        System.out.println(ColorfulConsole.RED);
+        System.out.println(ColorfulConsole.BLUE);
+        System.out.println(ColorfulConsole.GREEN);
+        System.out.println(ColorfulConsole.RESET);
+        System.out.println("I am changing colors in Java");
+    }
+}
