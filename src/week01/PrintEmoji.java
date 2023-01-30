c712532a9b5624c0fbdd5589f23e7c39c5fcfa7e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-01-29 23:15:00 -0500

    added emoji

--- src/week01/PrintEmoji.java
+++ src/week01/PrintEmoji.java
@@ -5,5 +5,10 @@ public class PrintEmoji {
     public static void main(String[] args) {
 
         System.out.println("\uD83E\uDD8A");
+        System.out.println("\uD83D\uDE01");
+        System.out.println("\uD83D\uDE02");
+        System.out.println("\uD83D\uDE03");
+        System.out.println("\uD83D\uDE04");
+        System.out.println("\uD83D\uDE05");
     }
 }
