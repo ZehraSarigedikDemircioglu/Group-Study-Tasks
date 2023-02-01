90c748669e52b5f7fa29b61882b0c6a79efeeb97
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-01 16:21:02 -0500

    added the task

---
+++ src/week02/FirstNonRepeatingCharacter.java
@@ -0,0 +1,17 @@
+package week02;
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "abcabcdef";
+
+        for(Character ch : str.toCharArray()) {
+            if(str.indexOf(ch) == str.lastIndexOf(ch)) {
+                System.out.println("First non repeat character: " + ch);
+                break;
+            }
+        }
+    }
+}
+
