b7ede724d8f083a33830131e96d004913b61af56
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-23 23:35:08 -0500

    week-05 added

---
+++ src/week05/StreamGetValuesFromMap.java
@@ -0,0 +1,24 @@
+package week05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StreamGetValuesFromMap {
+
+    public static void main(String[] args) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 2);
+        map.put(2, 2);
+        map.put(3, 2);
+        map.put(4, 2);
+        map.put(6, 3);
+
+        int result = map.entrySet().stream().filter(p -> p.getKey() % 2 == 0).mapToInt(p -> p.getValue()).reduce(Integer::sum).getAsInt();
+        System.out.println(result);
+
+    }
+}
+/*
+sum all values in my map which key%2==0
+ */
