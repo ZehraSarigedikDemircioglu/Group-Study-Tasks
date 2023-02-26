d8e42647de97dc21f46ee860223d93e18ed461fc
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-25 23:01:56 -0500

    week-05 added

--- src/week05/StreamGetValuesFromMap.java
+++ src/week05/StreamGetValuesFromMap.java
@@ -9,9 +9,9 @@ public class StreamGetValuesFromMap {
 
         Map<Integer, Integer> map = new HashMap<>();
         map.put(1, 2);
-        map.put(2, 2);
+        map.put(2, 12);
         map.put(3, 2);
-        map.put(4, 2);
+        map.put(4, 22);
         map.put(6, 3);
 
         int result = map.entrySet().stream().filter(p -> p.getKey() % 2 == 0).mapToInt(p -> p.getValue()).reduce(Integer::sum).getAsInt();
