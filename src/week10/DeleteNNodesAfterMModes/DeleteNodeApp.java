eff2c2b5c9550133776c16d7d8cf3e8c571a6f04
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-29 00:11:04 -0400

    self practice added

--- src/week10/DeleteNNodesAfterMModes/DeleteNodeApp.java
+++ src/week10/DeleteNNodesAfterMModes/DeleteNodeApp.java
@@ -14,5 +14,15 @@ public class DeleteNodeApp {
         list.printNodes(); // 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> 10=> 11=> 12=> 13=> null
         list.deleteNNodesAfterMModes(2, 3);
         list.printNodes(); // 1=> 2=> 6=> 7=> 11=> 12=> null
+
+        LinkedList list2 = new LinkedList();
+
+        for (int i = 1; i <= 11 ; i++) {
+            list2.add(i);
+        }
+
+        list2.printNodes(); // 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> 10=> 11=> null
+        list2.deleteNNodesAfterMModes(1, 3);
+        list2.printNodes(); // 1=> 2=> 6=> 7=> 11=> 12=> null
     }
 }
