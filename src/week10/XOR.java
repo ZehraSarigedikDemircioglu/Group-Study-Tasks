70029d934c3bd370f636d707509ca11cfb5cfea3
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-02 01:24:19 -0400

    self practice added

--- src/week10/XOR.java
+++ src/week10/XOR.java
@@ -7,7 +7,16 @@ public class XOR {
         int x = 5;
         int y = 3;
 
+        // |3 + 5 - 2 x 3 x 5|= 22 => 2x2^0+2x2^1 = 6
+
         System.out.println(x ^ y); // 6
+
+        int a = 5;
+        int b = 7;
+
+        // |5 + 7 - 2 x 5 x 7| = 58 => 8 x 2 ^ 0 + 5 x 2 ^ 1 = 18 => 8 x 2 ^ 0 + 1 x 2 ^ 1 = 10 => 1 x 2 ^ 1 = 2
+
+        System.out.println(a ^ b); // 2
 
         // If they are same, gives 0, if one of them zero, result will be other number
 
