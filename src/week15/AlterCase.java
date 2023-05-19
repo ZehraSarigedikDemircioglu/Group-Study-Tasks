fdf1f97437732c1bfd1810f9c84c2a6534216d2a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-19 15:12:09 -0400

    added week-15

---
+++ src/week15/AlterCase.java
@@ -0,0 +1,34 @@
+package week15;
+
+public class AlterCase {
+
+    private String alternateCase(String str, boolean isUpperCase) {
+
+        if (str.isEmpty()) {
+            return "";
+        }
+        char ch = str.charAt(0);
+        if (Character.isAlphabetic(ch)) {
+            if (isUpperCase) {
+                ch = Character.toUpperCase(ch);
+            } else {
+                ch = Character.toLowerCase(ch);
+            }
+            return ch + alternateCase(str.substring(1), !isUpperCase);
+        } else {
+            return ch + alternateCase(str.substring(1), isUpperCase);
+        }
+    }
+}
+/*
+Given a path of input file which includes sentence(s), write a program that reads alternates the case of every
+alphabetic character, starting with uppercase. Spaces and non-alphabetical characters should be added to the
+final output as is, i.e. they should not be taken into account when alternating between upper/lowercase.
+Input: Your program should read lines from file as a standard input. Each line contains a sentence. Assume all
+characters are ASCII.
+Output: For each sentence from standard input, return a string which contains a sentence in each line such that
+the first character is uppercase, the next character is lowercase and so on.
+Test 1
+Test Input: We are the world
+Expected Output: We ArE tHe WoRlD
+ */
