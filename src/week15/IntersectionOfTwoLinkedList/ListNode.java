b38d4adadc8058c412784b6e16c159c3be677d79
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-19 15:07:58 -0400

    added listnode for week-15

---
+++ src/week15/IntersectionOfTwoLinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package week15.IntersectionOfTwoLinkedList;
+
+public class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+}
