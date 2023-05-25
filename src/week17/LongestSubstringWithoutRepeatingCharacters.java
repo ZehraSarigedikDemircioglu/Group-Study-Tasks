b56e6a4f79ca47f9f08d05696fbd7d9120a3246b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-25 14:26:43 -0400

    added week-17

--- src/week17/LongestSubstringWithoutRepeatingCharacters.java
+++ src/week17/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,15 +1,35 @@
 package week17;
 
+import java.util.HashMap;
+
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
 
         System.out.println(lengthOfLongestSubstring("abcabcbb")); // 3
+        System.out.println(lengthOfLongestSubstring("bbbbb")); // 1
+        System.out.println(lengthOfLongestSubstring("pwwkew")); // 3
 
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        return 0;
+
+        if (s.length()==0){
+            return 0;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        int max=0;
+
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
     }
 }
 /*
