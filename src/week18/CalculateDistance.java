af399650c7d44825f54ad695eb632c59345305c9
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-29 23:10:35 -0400

    added week-18

---
+++ src/week18/CalculateDistance.java
@@ -0,0 +1,20 @@
+package week18;
+
+public class CalculateDistance {
+}
+
+/*
+You have (x, y) coordinates for 2 points and need to find the distance between them.
+Input:
+Your program should read lines from the file. Each line contains two coordinate pairs which are space-delimited.
+All x and y values are integers between -100 and 100.
+Output:
+Return the list of distance between the points. You do not need to round the results you receive.
+Test 1
+ (25, 4) (1, -6)
+Expected Output 26.0
+Test 2
+ (47, 43) (-25, -11)
+Expected Output 90.0
+Constraints: Input file will have at least one line with valid coordinates.
+ */
