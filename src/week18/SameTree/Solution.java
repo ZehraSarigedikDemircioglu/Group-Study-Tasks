5de738327234c45ae2bea19a344e1ae9db4a8789
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-10 23:29:54 -0400

    added week-18

--- src/week18/SameTree/Solution.java
+++ src/week18/SameTree/Solution.java
@@ -3,8 +3,12 @@ package week18.SameTree;
 public class Solution {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
-    }
+
+        if(p == null && q == null){
+            return true;
+        }
+            return p != null && q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
 }
 
 /*
