a54601c49b14f43ac4ad106a86a9b7e9deefa1a9
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-12 23:57:49 -0400

    added week-19

---
+++ src/week19/RangeSumOfBST/TreeNode.java
@@ -0,0 +1,21 @@
+package week19.RangeSumOfBST;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
